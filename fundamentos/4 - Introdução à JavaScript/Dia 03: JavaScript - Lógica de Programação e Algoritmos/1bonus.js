0a5011367ff6bbbf7ad5f41b25fedd808efd0c72
Vitor Veloso <vitor.asv@gmail.com>
2022-10-25 16:16:01 -0300

    Todos os bonus incluindo piramide vazada

--- fundamentos/4 - Introdução à JavaScript/Dia 03: JavaScript - Lógica de Programação e Algoritmos/1bonus.js
+++ fundamentos/4 - Introdução à JavaScript/Dia 03: JavaScript - Lógica de Programação e Algoritmos/1bonus.js
@@ -1,10 +1,10 @@
-let n = 7;
+let n = 21;
 
 if (n <= 1) {
     return 1;
 }
 
-trianguloCheio(n);
+trianguloVazado(n);
 
 function quadrado(n) {
     let array = [];
@@ -48,20 +48,51 @@ function trianguloCheio(n) {
 
     for (let i = 1; i <= n; i += 2) { // spaces + 1 = height // i = lines
         array = []; // reset array
-        for (let j = 0; j < spaces + 1 - i; j += 1) { // first three spaces
+        for (let j = 0; j < spaces; j += 1) { // first three spaces
             array.push(' ');
         }
         for (let k = 0; k < i; k += 1) { // prints *
             array.push('*');
         }
+        spaces -= 1;
+        console.log(array.join(''));
     }
-}
+};
 
-//    *    *1 = 1   espaços = altura - 1 de cada lado
-//   ***    *2 = *1 + 2
-//  *****   *3 = *2 + 2
+function trianguloVazado(n) {
+    if (n % 2 == 0) {
+        return 1;
+    }
+
+    let spaces = Math.floor(n/2);
+
+    for (let i = 1; i <= n; i += 2) { // spaces + 1 = height // i = lines
+        array = []; // reset array
+        for (let j = 0; j < spaces; j += 1) { // first three spaces
+            array.push(' ');
+        }
+        
+        if (i == 1) {
+            array.push('*'); // prints * for first line
+        } else if (i == n) {
+            for (let k = 0; k < n; k += 1) {
+                array.push('*');  // prints * for last line
+            }
+        } else { // conditions for lines in between
+            array.push('*'); // prints *
+            for (let k = 1; k <= i - 2; k += 1) { // prints spaces inside
+                array.push(' ');
+            }
+            array.push('*'); // prints *
+        }
+        spaces -= 1;
+        console.log(array.join(''));
+    }
+
+        
+};
+
+//    *
+//   * *
+//  *   *
 // *******
-
-//   *  
-//  ***
-// *****
