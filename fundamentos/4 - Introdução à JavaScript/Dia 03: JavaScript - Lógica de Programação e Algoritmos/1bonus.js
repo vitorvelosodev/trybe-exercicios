e5e1ce0cc475cfa6a82ca251d785cd1cba147c09
Vitor Veloso <vitor.asv@gmail.com>
2022-10-25 15:44:19 -0300

    Defining funcs for trianguloCheio

--- fundamentos/4 - Introdução à JavaScript/Dia 03: JavaScript - Lógica de Programação e Algoritmos/1bonus.js
+++ fundamentos/4 - Introdução à JavaScript/Dia 03: JavaScript - Lógica de Programação e Algoritmos/1bonus.js
@@ -1,10 +1,10 @@
-let n = 5;
+let n = 7;
 
 if (n <= 1) {
     return 1;
 }
 
-quadrado(n);
+trianguloCheio(n);
 
 function quadrado(n) {
     let array = [];
@@ -21,6 +21,47 @@ function quadrado(n) {
 function trianguloDir(n) {
     let array = [];
     for (let i = 0; i < n; i += 1) {
-        
+        array.push('*');
+        console.log(array.join(''));
     }
 };
+
+function trianguloEsq(n) {
+    for (let i = 0; i < n; i += 1) {
+        array = []; // reset array
+        for (let j = 0; j < (n - i - 1); j += 1) {
+            array.push(' ');
+        }
+        for (let j = 0; j <= i; j += 1) {
+            array.push('*');
+        }
+        console.log(array.join(''));
+    }
+};
+
+function trianguloCheio(n) {
+    if (n % 2 == 0) {
+        return 1;
+    }
+
+    let spaces = Math.floor(n/2);
+
+    for (let i = 1; i <= n; i += 2) { // spaces + 1 = height // i = lines
+        array = []; // reset array
+        for (let j = 0; j < spaces + 1 - i; j += 1) { // first three spaces
+            array.push(' ');
+        }
+        for (let k = 0; k < i; k += 1) { // prints *
+            array.push('*');
+        }
+    }
+}
+
+//    *    *1 = 1   espaços = altura - 1 de cada lado
+//   ***    *2 = *1 + 2
+//  *****   *3 = *2 + 2
+// *******
+
+//   *  
+//  ***
+// *****
